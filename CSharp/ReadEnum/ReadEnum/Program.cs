faa395e978c21c7d260231aefdde6259d9f7d894
何增福 <hzf@neibour.com>
2019-07-05 15:49:22 +0800

    Add. ReadEnum.

---
+++ CSharp/ReadEnum/ReadEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ReadEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GameType gameType = new GameType();
+            string[] gameTypeNameArray = Enum.GetNames(gameType.GetType());
+            Console.WriteLine("枚举个数：\t{0}", gameTypeNameArray.Length);
+            for (int i = 0; i < gameTypeNameArray.Length; i ++)
+            {
+                Console.WriteLine("{0}", gameTypeNameArray[i]);
+            }
+        }
+
+        public enum GameType
+        {
+            MMORPG,
+            RPG,
+            MOBA,
+            RTS,
+            FPS
+        }
+    }
+}
